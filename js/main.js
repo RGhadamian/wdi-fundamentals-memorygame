5f19c413e1585acbb9797299f1223a6a0058c45f
RGhadamian <roshan@ghadamian.com>
2019-07-28 17:20:55 +1000

    Refactoring main.js file for re-usable functions

--- js/main.js
+++ js/main.js
@@ -2,22 +2,21 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+function checkForMatch () {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log('You found a match');
+    } else {
+      console.log('Sorry, try again');
+    }
+};
+};
+function flipCard (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match');
-  } else {
-    alert('Sorry, try again');
-  }
-}
+flipCard(0);
+flipCard(2);
+console.log(cardsInPlay);
